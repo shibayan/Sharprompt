30b8d1bdadbce2b12d514a10af158e9025ddca3d
shibayan <me@shibayan.jp>
2019-08-03 00:14:50 +0900

    Adding Select filtering

--- Sharprompt/Select.cs
+++ Sharprompt/Select.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Sharprompt
 {
@@ -11,20 +13,28 @@ namespace Sharprompt
             _options = options;
             _defaultValue = defaultValue;
             _labelSelector = labelSelector ?? (x => x.ToString());
+            _filtering = (filter, value) => value.Contains(filter);
+
+            _filteredOptions = FilteredOptions();
         }
 
         private readonly string _message;
         private readonly IReadOnlyList<T> _options;
         private readonly object _defaultValue;
         private readonly Func<T, string> _labelSelector;
+        private readonly Func<string, string, bool> _filtering;
 
         private int _selectedIndex;
+        private StringBuilder _filter;
+        private IReadOnlyList<T> _filteredOptions;
 
         public T Start()
         {
             using (var scope = new ConsoleScope(false))
             {
                 _selectedIndex = FindIndex();
+
+                _filter = new StringBuilder(64);
 
                 while (true)
                 {
@@ -43,7 +53,7 @@ namespace Sharprompt
                     }
                     else if (keyInfo.Key == ConsoleKey.DownArrow)
                     {
-                        if (_selectedIndex >= _options.Count - 1)
+                        if (_selectedIndex >= _filteredOptions.Count - 1)
                         {
                             _selectedIndex = 0;
                         }
@@ -56,28 +66,55 @@ namespace Sharprompt
                     {
                         if (_selectedIndex <= 0)
                         {
-                            _selectedIndex = _options.Count - 1;
+                            _selectedIndex = _filteredOptions.Count - 1;
                         }
                         else
                         {
                             _selectedIndex--;
                         }
                     }
+                    else if (keyInfo.Key == ConsoleKey.Backspace)
+                    {
+                        if (_filter.Length == 0)
+                        {
+                            scope.Beep();
+                        }
+                        else
+                        {
+                            _filter.Length -= 1;
+
+                            _filteredOptions = FilteredOptions();
+                            _selectedIndex = -1;
+                        }
+                    }
+                    else if (!char.IsControl(keyInfo.KeyChar))
+                    {
+                        _filter.Append(keyInfo.KeyChar);
+
+                        _filteredOptions = FilteredOptions();
+                        _selectedIndex = -1;
+                    }
                 }
 
                 scope.Render(FinishTemplate);
 
-                return _options[_selectedIndex];
+                return _filteredOptions[_selectedIndex];
             }
         }
 
         private void Template(ConsoleRenderer renderer)
         {
             renderer.WriteMessage(_message);
+            renderer.Write(_filter.ToString());
 
-            for (int i = 0; i < _options.Count; i++)
+            for (int i = 0; i < _filteredOptions.Count; i++)
             {
-                var label = _labelSelector(_options[i]);
+                var label = _labelSelector(_filteredOptions[i]);
+
+                if (!label.Contains(_filter.ToString()))
+                {
+                    continue;
+                }
 
                 renderer.WriteLine();
 
@@ -95,7 +132,7 @@ namespace Sharprompt
         private void FinishTemplate(ConsoleRenderer renderer)
         {
             renderer.WriteMessage(_message);
-            renderer.Write(_labelSelector(_options[_selectedIndex]), ConsoleColor.Cyan);
+            renderer.Write(_labelSelector(_filteredOptions[_selectedIndex]), ConsoleColor.Cyan);
         }
 
         private int FindIndex()
@@ -105,15 +142,28 @@ namespace Sharprompt
                 return -1;
             }
 
-            for (int i = 0; i < _options.Count; i++)
+            for (int i = 0; i < _filteredOptions.Count; i++)
             {
-                if (_defaultValue.Equals(_options[i]))
+                if (_defaultValue.Equals(_filteredOptions[i]))
                 {
                     return i;
                 }
             }
 
             return -1;
+        }
+
+        private IReadOnlyList<T> FilteredOptions()
+        {
+            if (_filter == null || _filter.Length == 0)
+            {
+                return _options;
+            }
+
+            var filter = _filter.ToString();
+
+            return _options.Where(x => _filtering(filter, _labelSelector(x)))
+                           .ToArray();
         }
     }
 }
