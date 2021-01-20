7cfc45ab46e6204a9cf90728c19206aa52520548
Tatsuro Shibamura <me@shibayan.jp>
2021-01-21 00:08:28 +0900

    Fixed to explicitly enable VT100 on Windows

---
+++ Sharprompt/Internal/NativeMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sharprompt.Internal
+{
+    internal static class NativeMethods
+    {
+        public const int STD_OUTPUT_HANDLE = -11;
+        public const int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool GetConsoleMode(IntPtr hConsoleHandle, out int dwMode);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool SetConsoleMode(IntPtr hConsoleHandle, int dwMode);
+    }
+}
