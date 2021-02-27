b1aa26e9fa7900d9396d5727d326f23d3a2c5608
Tatsuro Shibamura <me@shibayan.jp>
2021-02-28 01:17:03 +0900

    Update naming rule (#110)

--- Sharprompt/Forms/MultiSelect.cs
+++ Sharprompt/Forms/MultiSelect.cs
@@ -10,7 +10,7 @@ namespace Sharprompt.Forms
 {
     internal class MultiSelect<T> : FormBase<IEnumerable<T>>
     {
-        public MultiSelect(string message, IEnumerable<T> items, int? pageSize, int minimum, int maximum, Func<T, string> valueSelector)
+        public MultiSelect(string message, IEnumerable<T> items, int? pageSize, int minimum, int maximum, Func<T, string> textSelector)
             : base(false)
         {
             // throw early when invalid options are passed
@@ -25,17 +25,17 @@ namespace Sharprompt.Forms
             }
 
             _message = message;
-            _selector = new Selector<T>(items, pageSize, Optional<T>.Empty, valueSelector);
+            _paginator = new Paginator<T>(items, pageSize, Optional<T>.Empty, textSelector);
             _minimum = minimum;
             _maximum = maximum;
-            _valueSelector = valueSelector;
+            _textSelector = textSelector;
         }
 
         private readonly string _message;
-        private readonly Selector<T> _selector;
+        private readonly Paginator<T> _paginator;
         private readonly int _minimum;
         private readonly int _maximum;
-        private readonly Func<T, string> _valueSelector;
+        private readonly Func<T, string> _textSelector;
 
         private readonly IList<T> _selectedItems = new List<T>();
         private readonly StringBuilder _filterBuffer = new StringBuilder();
@@ -52,7 +52,7 @@ namespace Sharprompt.Forms
                 case ConsoleKey.Enter:
                     Renderer.SetValidationResult(new ValidationResult($"A minimum selection of {_minimum} items is required"));
                     break;
-                case ConsoleKey.Spacebar when _selector.TryGetSelectedItem(out var currentItem):
+                case ConsoleKey.Spacebar when _paginator.TryGetSelectedItem(out var currentItem):
                 {
                     if (_selectedItems.Contains(currentItem))
                     {
@@ -66,16 +66,16 @@ namespace Sharprompt.Forms
                     break;
                 }
                 case ConsoleKey.UpArrow:
-                    _selector.PreviousItem();
+                    _paginator.PreviousItem();
                     break;
                 case ConsoleKey.DownArrow:
-                    _selector.NextItem();
+                    _paginator.NextItem();
                     break;
                 case ConsoleKey.LeftArrow:
-                    _selector.PreviousPage();
+                    _paginator.PreviousPage();
                     break;
                 case ConsoleKey.RightArrow:
-                    _selector.NextPage();
+                    _paginator.NextPage();
                     break;
                 case ConsoleKey.Backspace when _filterBuffer.Length == 0:
                     ConsoleDriver.Beep();
@@ -83,7 +83,7 @@ namespace Sharprompt.Forms
                 case ConsoleKey.Backspace:
                     _filterBuffer.Length -= 1;
 
-                    _selector.UpdateFilter(_filterBuffer.ToString());
+                    _paginator.UpdateFilter(_filterBuffer.ToString());
                     break;
                 default:
                 {
@@ -91,7 +91,7 @@ namespace Sharprompt.Forms
                     {
                         _filterBuffer.Append(keyInfo.KeyChar);
 
-                        _selector.UpdateFilter(_filterBuffer.ToString());
+                        _paginator.UpdateFilter(_filterBuffer.ToString());
                     }
 
                     break;
@@ -106,22 +106,22 @@ namespace Sharprompt.Forms
         protected override void InputTemplate(ScreenBuffer screenBuffer)
         {
             screenBuffer.WritePrompt(_message);
-            screenBuffer.Write(_selector.FilterTerm);
+            screenBuffer.Write(_paginator.FilterTerm);
 
-            if (string.IsNullOrEmpty(_selector.FilterTerm))
+            if (string.IsNullOrEmpty(_paginator.FilterTerm))
             {
                 screenBuffer.Write(" Hit space to select", Prompt.ColorSchema.Answer);
             }
 
-            var subset = _selector.ToSubset();
+            var subset = _paginator.ToSubset();
 
             foreach (var item in subset)
             {
-                var value = _valueSelector(item);
+                var value = _textSelector(item);
 
                 screenBuffer.WriteLine();
 
-                if (_selector.TryGetSelectedItem(out var selectedItem) && EqualityComparer<T>.Default.Equals(item, selectedItem))
+                if (_paginator.TryGetSelectedItem(out var selectedItem) && EqualityComparer<T>.Default.Equals(item, selectedItem))
                 {
                     if (_selectedItems.Contains(item))
                     {
@@ -149,7 +149,7 @@ namespace Sharprompt.Forms
         protected override void FinishTemplate(ScreenBuffer screenBuffer, IEnumerable<T> result)
         {
             screenBuffer.WriteFinish(_message);
-            screenBuffer.Write(string.Join(", ", result.Select(_valueSelector)), Prompt.ColorSchema.Answer);
+            screenBuffer.Write(string.Join(", ", result.Select(_textSelector)), Prompt.ColorSchema.Answer);
         }
     }
 }
