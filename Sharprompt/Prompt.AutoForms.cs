1937b61edc6d2c7ea6d7195de2719d2c78bd7455
Tatsuro Shibamura <me@shibayan.jp>
2021-10-07 02:22:26 +0900

    Refactoring `AutoForm` implementation

--- Sharprompt/Prompt.AutoForms.cs
+++ Sharprompt/Prompt.AutoForms.cs
@@ -28,50 +28,73 @@ namespace Sharprompt
 
         private static void StartForms<T>(T model)
         {
-            var propertyMetadatas = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                             .Select(x => new PropertyMetadata(x))
-                                             .OrderBy(x => x.Order)
-                                             .ToArray();
+            var propertyMetadatas = PropertyMetadataFactory.Create<T>();
 
             foreach (var propertyMetadata in propertyMetadatas)
             {
                 var propertyInfo = propertyMetadata.PropertyInfo;
-                var validators = propertyMetadata.Validations.Select(x => new ValidationAttributeAdapter(x).GetValidator(propertyInfo.Name, model)).ToArray();
 
                 var defaultValue = propertyInfo.GetValue(model);
+                var validators = propertyMetadata.GetValidator(model);
 
                 if (propertyMetadata.DataType == DataType.Password)
                 {
-                    propertyInfo.SetValue(model, Password(propertyMetadata.Prompt, validators: validators));
+                    var options = new PasswordOptions
+                    {
+                        Message = propertyMetadata.Message
+                    };
+
+                    options.Validators.Merge(validators);
+
+                    propertyInfo.SetValue(model, Password(options));
                 }
                 else if (propertyMetadata.PropertyType == typeof(bool))
                 {
-                    propertyInfo.SetValue(model, Confirm(propertyMetadata.Prompt, (bool?)defaultValue));
+                    var options = new ConfirmOptions
+                    {
+                        Message = propertyMetadata.Message,
+                        DefaultValue = (bool?)defaultValue
+                    };
+
+                    propertyInfo.SetValue(model, Confirm(options));
                 }
                 else if (propertyMetadata.PropertyType.IsEnum)
                 {
                     var method = _selectMethod.MakeGenericMethod(propertyMetadata.PropertyType);
 
-                    propertyInfo.SetValue(model, InvokeMethod(method, propertyMetadata.Prompt, null, defaultValue));
+                    propertyInfo.SetValue(model, InvokeMethod(method, propertyMetadata.Message, null, defaultValue));
                 }
                 else if (propertyMetadata.IsCollection && propertyMetadata.PropertyType.GetGenericArguments()[0].IsEnum)
                 {
                     var method = _multiSelectMethod.MakeGenericMethod(propertyMetadata.PropertyType.GetGenericArguments()[0]);
 
-                    propertyInfo.SetValue(model, InvokeMethod(method, propertyMetadata.Prompt, null, 1, int.MaxValue, defaultValue));
+                    propertyInfo.SetValue(model, InvokeMethod(method, propertyMetadata.Message, null, 1, int.MaxValue, defaultValue));
                 }
                 else
                 {
                     var method = _inputMethod.MakeGenericMethod(propertyMetadata.PropertyType);
 
-                    propertyInfo.SetValue(model, InvokeMethod(method, propertyMetadata.Prompt, defaultValue, validators));
+                    propertyInfo.SetValue(model, InvokeMethod(method, propertyMetadata.Message, defaultValue, validators));
                 }
+
+                propertyInfo.SetValue(model, null);
             }
         }
 
         private static object InvokeMethod(MethodInfo methodInfo, params object[] parameters)
         {
             return methodInfo.Invoke(null, parameters);
+        }
+
+        private static class PropertyMetadataFactory
+        {
+            public static IReadOnlyList<PropertyMetadata> Create<T>()
+            {
+                return typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                .Select(x => new PropertyMetadata(x))
+                                .OrderBy(x => x.Order)
+                                .ToArray();
+            }
         }
 
         private class PropertyMetadata
@@ -85,7 +108,7 @@ namespace Sharprompt
                 PropertyType = Nullable.GetUnderlyingType(propertyInfo.PropertyType) ?? propertyInfo.PropertyType;
                 DataType = dataTypeAttribute?.DataType;
                 IsCollection = propertyInfo.PropertyType.IsGenericType && propertyInfo.PropertyType.GetGenericTypeDefinition() == typeof(IEnumerable<>);
-                Prompt = displayAttribute?.GetPrompt();
+                Message = displayAttribute?.GetPrompt();
                 Order = displayAttribute?.GetOrder();
                 Validations = propertyInfo.GetCustomAttributes<ValidationAttribute>(true);
             }
@@ -94,9 +117,13 @@ namespace Sharprompt
             public Type PropertyType { get; }
             public DataType? DataType { get; }
             public bool IsCollection { get; }
-            public string Prompt { get; }
+            public string Message { get; }
             public int? Order { get; }
-            public IEnumerable<ValidationAttribute> Validations { get; }
+
+            private IEnumerable<ValidationAttribute> Validations { get; }
+
+            public IReadOnlyList<Func<object, ValidationResult>> GetValidator(object model) =>
+                Validations.Select(x => new ValidationAttributeAdapter(x).GetValidator(PropertyInfo.Name, model)).ToArray();
         }
 
         private static readonly MethodInfo _inputMethod = typeof(Prompt).GetMethods().First(x => x.Name == nameof(Input) && x.GetParameters().Length == 3);
