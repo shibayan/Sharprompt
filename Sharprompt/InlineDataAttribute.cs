7cac645c525dc2b1e0888ad88f95e582e50953e0
Tatsuro Shibamura <me@shibayan.jp>
2021-12-08 21:21:10 +0900

    Adding data source attributes

---
+++ Sharprompt/InlineDataAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sharprompt
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class InlineDataAttribute : Attribute
+    {
+        public InlineDataAttribute(params object[] source)
+        {
+            _source = source;
+        }
+
+        private readonly object[] _source;
+    }
+}
---
+++ Sharprompt/MemberDataAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sharprompt
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class MemberDataAttribute : Attribute
+    {
+        public MemberDataAttribute(Type type, string memberName)
+        {
+            _type = type;
+            _memberName = memberName;
+        }
+
+        private readonly Type _type;
+        private readonly string _memberName;
+    }
+}
