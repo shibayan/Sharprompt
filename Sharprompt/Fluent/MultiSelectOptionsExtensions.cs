0ef5b3273c9536d8abc06ce12a9335eda2f0089b
Tatsuro Shibamura <me@shibayan.jp>
2021-12-10 20:19:13 +0900

    Adding fluent interface in Options class (#193)
    * Adding fluent interface in Options class
    
    * Move fluent-interface to extension methods
    
    * Refactoring fluent methods

---
+++ Sharprompt/Fluent/MultiSelectOptionsExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sharprompt.Fluent
+{
+    public static class MultiSelectOptionsExtensions
+    {
+        public static MultiSelectOptions<T> WithMessage<T>(this MultiSelectOptions<T> options, string message)
+        {
+            options.Message = message;
+
+            return options;
+        }
+
+        public static MultiSelectOptions<T> WithItems<T>(this MultiSelectOptions<T> options, IEnumerable<T> items)
+        {
+            options.Items = items;
+
+            return options;
+        }
+
+        public static MultiSelectOptions<T> WithDefaultValues<T>(this MultiSelectOptions<T> options, IEnumerable<T> defaultValues)
+        {
+            options.DefaultValues = defaultValues;
+
+            return options;
+        }
+
+        public static MultiSelectOptions<T> WithPageSize<T>(this MultiSelectOptions<T> options, int pageSize)
+        {
+            options.PageSize = pageSize;
+
+            return options;
+        }
+
+        public static MultiSelectOptions<T> WithMinimum<T>(this MultiSelectOptions<T> options, int minimum)
+        {
+            options.Minimum = minimum;
+
+            return options;
+        }
+
+        public static MultiSelectOptions<T> WithMaximum<T>(this MultiSelectOptions<T> options, int maximum)
+        {
+            options.Maximum = maximum;
+
+            return options;
+        }
+
+        public static MultiSelectOptions<T> WithTextSelector<T>(this MultiSelectOptions<T> options, Func<T, string> textSelector)
+        {
+            options.TextSelector = textSelector;
+
+            return options;
+        }
+
+        public static MultiSelectOptions<T> WithPagination<T>(this MultiSelectOptions<T> options, Func<int, int, int, string> pagination)
+        {
+            options.Pagination = pagination;
+
+            return options;
+        }
+    }
+}
---
+++ Sharprompt/Fluent/SelectOptionsExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sharprompt.Fluent
+{
+    public static class SelectOptionsExtensions
+    {
+        public static SelectOptions<T> WithMessage<T>(this SelectOptions<T> options, string message)
+        {
+            options.Message = message;
+
+            return options;
+        }
+
+        public static SelectOptions<T> WithItems<T>(this SelectOptions<T> options, IEnumerable<T> items)
+        {
+            options.Items = items;
+
+            return options;
+        }
+
+        public static SelectOptions<T> WithDefaultValue<T>(this SelectOptions<T> options, T defaultValue)
+        {
+            options.DefaultValue = defaultValue;
+
+            return options;
+        }
+
+        public static SelectOptions<T> WithPageSize<T>(this SelectOptions<T> options, int pageSize)
+        {
+            options.PageSize = pageSize;
+
+            return options;
+        }
+
+        public static SelectOptions<T> WithTextSelector<T>(this SelectOptions<T> options, Func<T, string> textSelector)
+        {
+            options.TextSelector = textSelector;
+
+            return options;
+        }
+
+        public static SelectOptions<T> WithPagination<T>(this SelectOptions<T> options, Func<int, int, int, string> pagination)
+        {
+            options.Pagination = pagination;
+
+            return options;
+        }
+    }
+}
